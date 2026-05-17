41a1de39cac4a6ef840886d63afec8616b350a90
billvanleeuwen424 <billvanleeuwen424@gmail.com>
2026-05-17 13:59:05 -0400

    Adding a basic echo tcp server

--- hello.go
+++ hello.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Strarting server")
+
+	listener, err := net.Listen("tcp", ":8080")
+
+	// check for errors on listening
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		go func(conn net.Conn) {
+
+			log.Println("Connection from", conn.RemoteAddr())
+			io.Copy(conn, conn)
+			conn.Close()
+			log.Println("Connection closed", conn.RemoteAddr())
+		}(conn)
+
+	}
 }
