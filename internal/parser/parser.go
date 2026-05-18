a49fa8ca60bbbc67bcf193ff40b4f94233df00ba
billvanleeuwen424 <billvanleeuwen424@gmail.com>
2026-05-18 19:11:34 -0400

    Handle a sub request in parser
    Testing Info:
    
    Server side:
    Starting server
    2026/05/18 19:10:49 Connection from [::1]:49228
    got command: {1 test}
    2026/05/18 19:10:59 parse error: bad input
    2026/05/18 19:11:02 parse error: bad input
    2026/05/18 19:11:04 Connection closed [::1]:49228
    ^Csignal: interrupt
    
    Client Side:
    SUB test 1
    SUB
    SUB test
    ^C

---
+++ internal/parser/parser.go
@@ -0,0 +1,44 @@
+package parser
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type SubCommand struct {
+	Sid   int
+	Topic string
+}
+
+type Command interface {
+	CommandType()
+}
+
+func (s SubCommand) CommandType() {}
+
+func ParseCommand(command string) (Command, error) {
+
+	words := strings.Fields(command)
+
+	if len(words) == 0 {
+		return nil, fmt.Errorf("bad input")
+	}
+
+	if words[0] == "SUB" {
+
+		if len(words) < 3 {
+			return nil, fmt.Errorf("bad input")
+		}
+
+		subid, err := strconv.Atoi(words[2])
+		if err != nil {
+			return nil, err
+		}
+
+		return SubCommand{subid, words[1]}, nil
+	}
+
+	return nil, fmt.Errorf("unknown command, not implemented: %s", words[0])
+
+}
